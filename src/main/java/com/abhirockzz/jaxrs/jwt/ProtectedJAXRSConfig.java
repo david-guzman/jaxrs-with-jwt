ecf42b310bda080fcaa8e563146e0f83d2a1cfff
David Guzman <d.guzman@ucl.ac.uk>
2017-08-01 22:44:22 +0100

    Added changes as in https://abhirockzz.wordpress.com/2016/03/21/jax-rs-jwt-a-pinch-of-jsr-375/

--- src/main/java/com/abhirockzz/jaxrs/jwt/ProtectedJAXRSConfig.java
+++ src/main/java/com/abhirockzz/jaxrs/jwt/ProtectedJAXRSConfig.java
@@ -1,8 +1,12 @@
 package com.abhirockzz.jaxrs.jwt;
 
 import com.abhirockzz.jaxrs.jwt.boundary.AuthResource;
+import org.glassfish.soteria.identitystores.annotation.Credentials;
+import org.glassfish.soteria.identitystores.annotation.EmbeddedIdentityStoreDefinition;
+
 import java.util.HashSet;
 import java.util.Set;
+import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -11,6 +15,10 @@ import javax.ws.rs.core.Application;
  * specific to /auth/token
  */
 @ApplicationPath("auth")
+@BasicAuthenticationMechanismDefinition(realmName = "file")
+@EmbeddedIdentityStoreDefinition({
+        @Credentials(callerName = "user1", password = "user1Password", groups = {"users"})
+})
 public class ProtectedJAXRSConfig extends Application {
 
     @Override
